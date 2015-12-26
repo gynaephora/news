d607ead5a8593d234b789277d2d554e23282c95e
gynaephora <it.mustango@gmail.com>
2015-12-26 14:07:18 +0200

    initial project version

---
+++ app/src/main/java/com/example/mustango/newsclient/NewsClientActivity.java
@@ -0,0 +1,13 @@
+package com.example.mustango.newsclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_client);
+    }
+}
